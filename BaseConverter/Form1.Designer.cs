5b2d20a62f07e57cd46b8d5d448f0cbd9e73a34a
Caleb Anderson <canderson@insidesales.com>
2015-09-03 07:11:58 -0600

    Initial Commit

---
+++ BaseConverter/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace BaseConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbFrom = new System.Windows.Forms.ComboBox();
+            this.cmbTo = new System.Windows.Forms.ComboBox();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbFrom
+            // 
+            this.cmbFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFrom.FormattingEnabled = true;
+            this.cmbFrom.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36"});
+            this.cmbFrom.Location = new System.Drawing.Point(55, 82);
+            this.cmbFrom.Name = "cmbFrom";
+            this.cmbFrom.Size = new System.Drawing.Size(121, 21);
+            this.cmbFrom.TabIndex = 2;
+            // 
+            // cmbTo
+            // 
+            this.cmbTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTo.FormattingEnabled = true;
+            this.cmbTo.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36"});
+            this.cmbTo.Location = new System.Drawing.Point(55, 115);
+            this.cmbTo.Name = "cmbTo";
+            this.cmbTo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTo.TabIndex = 5;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(16, 82);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(33, 13);
+            this.lblFrom.TabIndex = 1;
+            this.lblFrom.Text = "From:";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(16, 119);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(23, 13);
+            this.lblTo.TabIndex = 4;
+            this.lblTo.Text = "To:";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(182, 83);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(100, 20);
+            this.txtFrom.TabIndex = 3;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(182, 116);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(100, 20);
+            this.txtTo.TabIndex = 6;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(113, 150);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 7;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblInstructions.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblInstructions.Location = new System.Drawing.Point(12, 9);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(270, 70);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "To use this program, select the base types that you want to use then type a numbe" +
+    "r in the selected box.";
+            this.lblInstructions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 182);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.cmbTo);
+            this.Controls.Add(this.cmbFrom);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Base Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbFrom;
+        private System.Windows.Forms.ComboBox cmbTo;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblInstructions;
+    }
+}
+
---
+++ BaseConverter/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseConverter
+{
+    public partial class Form1 : Form
+    {
+        const string CONVERSION_STRING = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private enum ComboBoxValues : int
+        {
+            _02 = 0,
+            _03 = 1,
+            _04 = 2,
+            _05 = 3,
+            _06 = 4,
+            _07 = 5,
+            _08 = 6,
+            _09 = 7,
+            _10 = 8,
+            _11 = 9,
+            _12 = 10,
+            _13 = 11,
+            _14 = 12,
+            _15 = 13,
+            _16 = 14,
+            _17 = 15,
+            _18 = 16,
+            _19 = 17,
+            _20 = 18,
+            _21 = 19,
+            _22 = 20,
+            _23 = 21,
+            _24 = 22,
+            _25 = 23,
+            _26 = 24,
+            _27 = 25,
+            _28 = 26,
+            _29 = 27,
+            _30 = 28,
+            _31 = 29,
+            _32 = 30,
+            _33 = 31,
+            _34 = 32,
+            _35 = 33,
+            _36 = 34
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbFrom.SelectedIndex = (int)ComboBoxValues._10;
+            cmbTo.SelectedIndex = (int)ComboBoxValues._36;
+        }
+
+        private int ConvertToBase10(string str)
+        {
+            int workingValue = 0;
+
+            for (int j = str.Length - 1; j >= 0; j-- )
+            {
+                for (int i = 0; i < CONVERSION_STRING.Length; i++)
+                {
+                    if (str[j] == CONVERSION_STRING[i])
+                    {
+                        workingValue += i * (int)Math.Pow(10, j);
+                        break;
+                    }
+                }
+            }
+
+            return workingValue;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            int convertedArray = ConvertToBase10(txtFrom.Text);
+
+            txtTo.Text = ConvertToBase(int.Parse(txtFrom.Text), 36);
+        }
+
+        public String ConvertToBase(int num, int nbase)
+        {
+            String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            // check if we can convert to another base
+            if (nbase < 2 || nbase > chars.Length)
+                return "";
+
+            int r;
+            String newNumber = "";
+
+            // in r we have the offset of the char that was converted to the new base
+            while (num >= nbase)
+            {
+                r = num % nbase;
+                newNumber = chars[r] + newNumber;
+                num = num / nbase;
+            }
+            // the last number to convert
+            newNumber = chars[num] + newNumber;
+
+            return newNumber;
+        }
+    }
+}
